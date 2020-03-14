cdd43786e2f73d62892ec9a648b20434362093ed
Katarzyna <kkazimierczuk@gmail.com>
2020-03-14 17:02:42 +0100

    addinitAccordion function

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
-
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
     renderInMenu(){
@@ -78,6 +78,47 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion(){
+      const thisProduct = this;
+  
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickabletrigger:',clickableTrigger);
+  
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function(){
+        console.log('spr:',thisProduct.accordionTrigger);
+  
+        /* prevent default action for event */
+        event.preventDefault();
+  
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle('active');
+  
+        /* find all active products */
+        const allActiveProducts = document.querySelectorAll('article.active');
+        
+        /* START LOOP: for each active product */
+        for(let activeProduct of allActiveProducts ){
+          /* START: if the active product isn't the element of thisProduct */
+
+          if( activeProduct !== thisProduct.element){
+            
+            /* remove class active for the active product */
+            activeProduct.classList.remove('active');
+          }
+          /* END: if the active product isn't the element of thisProduct */
+        }
+  
+        /* END LOOP: for each active product */  
+        
+      });
+  
+      /* END: click event listener to trigger */
+      
+    }
+
   }
 
   const app = {
@@ -107,6 +148,5 @@
     },
   };
   
-
   app.init();
 }
