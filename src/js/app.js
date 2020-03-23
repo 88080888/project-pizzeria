971405eb5ef87156e874a321ec2a2a885de47bec
Katarzyna <kkazimierczuk@gmail.com>
2020-03-23 14:14:59 +0100

    add new component Booking.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    //przechowyjemy wszystkie kontenery podstron ktore musimy wyszukac w drzewie dom / dzięki children we wlasciwosci pages znajda sie wszystkie dzieci kontenera stron
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/','');
+   
+    //aktywacja pierwszej podstony
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click',function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href atribute */
+        // w stalej id zapisuje atrybut href kliknietego elementu w ktorym zamieniam znak # na pusty ciag znakow
+        const id = clickedElement.getAttribute('href').replace('#','');
+
+        /* run thisApp.activatePage with that id*/
+        thisApp.activatePage(id);
+
+        /*change URL hash*/
+        window.location.hash = '#/' + id;
+
+      });
+    }
+  },
+
+  //nadanie klasy active  wrapperowi pages i booking
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /*add class "active" to matching pages, remove from non-matching*/
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /*add class "active" to matching links, remove from non-matching*/
+    //dla kazdego z linkow zapisanych w thisApp.navLinks  dodaj lub usuń klasę zdefiniowaną w classNames.nav.active  i w zaleznosci od tego czy atrybut href tego linka jest rowny # + pageId
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu(){
     const thisApp = this;
     //console.log('thisApp.data:', thisApp.data);
@@ -24,7 +84,7 @@ const app = {
       })
     // otrzymaną odpowiedź konwertujemy z JSONa na tablicę
       .then(function(parsedResponse){
-        console.log('parasedResponse', parsedResponse);
+        //console.log('parasedResponse', parsedResponse);
 
         /*save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
@@ -32,7 +92,7 @@ const app = {
         /* execute initMenu method */
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));    
+    //console.log('thisApp.data', JSON.stringify(thisApp.data));    
   },
 
   //inicjuje instancję koszyka
@@ -49,6 +109,12 @@ const app = {
     });
   },
 
+  initBooking(){
+    const thisApp = this;
+
+    const bookingWrapper = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWrapper);
+  },
 
   init: function(){
     const thisApp = this;
@@ -58,10 +124,13 @@ const app = {
     //console.log('settings:', settings);
     //console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     //thisApp.initMenu();
     thisApp.initCart();
+    thisApp.initBooking();
   },
+
 };
   
 app.init();
